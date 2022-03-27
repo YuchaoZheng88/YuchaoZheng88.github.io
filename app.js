d7f992fa2a4853c842ccebcad8a894bf177c6d51
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-27 08:25:10 -0400

    sub

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 /* Registering Service Worker */
-/*  tttest  */
-
 if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 };
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220327.1505';
+const cacheName = 'chirpy-20220327.2024';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
