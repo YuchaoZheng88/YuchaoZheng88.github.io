8da7f40b0f38abec2849a6fcb9c80b689bd68462
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-28 08:49:28 -0400

    2sum and change date 2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220328.2045';
+const cacheName = 'chirpy-20220328.2049';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
