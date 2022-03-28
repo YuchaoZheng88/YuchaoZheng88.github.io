5db85588a1ac825e91b09136a3e95e83babb59c3
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-27 23:23:07 -0400

    delete old posts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220328.1017';
+const cacheName = 'chirpy-20220328.1115';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
