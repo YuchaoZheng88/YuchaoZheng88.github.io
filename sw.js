1ee1ac8e76c62e317f9ddb15c2c8d38a2a873ca2
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-29 08:20:39 -0400

    htbsecret

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220329.1032';
+const cacheName = 'chirpy-20220329.2020';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
