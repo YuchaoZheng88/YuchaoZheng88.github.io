b2111cb786dd41c32094b63738b8d1fa4a7e0984
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-28 22:32:58 -0400

    postnew

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220328.2315';
+const cacheName = 'chirpy-20220329.1032';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
