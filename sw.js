115bf239a4f7e0ff5c2fc42c7d215eb6802593b5
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-31 08:59:22 -0400

    stacked add comment

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220331.2053';
+const cacheName = 'chirpy-20220331.2059';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
