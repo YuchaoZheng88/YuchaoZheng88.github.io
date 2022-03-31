ce2e971a16816bfec36545034b34f944157f2989
YuchaoZheng88 <yc236984611@gmail.com>
2022-03-31 08:53:41 -0400

    stacked privesc

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220331.1119';
+const cacheName = 'chirpy-20220331.2053';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
