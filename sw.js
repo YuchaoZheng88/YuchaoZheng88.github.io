395fff6169269a125a3a760b0abab1d36e1291eb
YuchaoZheng88 <yc236984611@gmail.com>
2022-05-06 10:50:21 -0400

    makespace

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220501.2242';
+const cacheName = 'chirpy-20220506.2250';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
