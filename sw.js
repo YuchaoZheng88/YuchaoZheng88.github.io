4c6cac46b1c8e4b2c7d46f0c96ca4b3cc3d9e75d
YuchaoZheng88 <yc236984611@gmail.com>
2022-05-13 10:44:58 -0400

    crypto update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220513.2233';
+const cacheName = 'chirpy-20220513.2244';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
