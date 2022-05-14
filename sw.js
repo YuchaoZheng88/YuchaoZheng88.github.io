b2616180717e73a3938f3f31705d30fdcaa5fea9
YuchaoZheng88 <yc236984611@gmail.com>
2022-05-14 10:11:26 -0400

    beauty

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220514.2202';
+const cacheName = 'chirpy-20220514.2211';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
