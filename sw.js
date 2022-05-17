cf50ccb44ca7daf660393bd3c911d5c6c33b6980
YuchaoZheng88 <yc236984611@gmail.com>
2022-05-17 09:07:44 -0400

    coffee

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220516.2149';
+const cacheName = 'chirpy-20220517.2107';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
