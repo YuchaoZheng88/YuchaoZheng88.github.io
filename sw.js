11d1c56f7654141f6986ea834e48c1d5f2cea2ba
YuchaoZheng88 <yc236984611@gmail.com>
2022-05-24 09:11:27 -0400

    linuxforctf

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220522.2231';
+const cacheName = 'chirpy-20220524.2111';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
