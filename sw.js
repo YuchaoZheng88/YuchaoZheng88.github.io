f1f9b42017390a5e459fa882ba2d1ef0a6cc64e1
YuchaoZheng88 <yc236984611@gmail.com>
2022-07-11 10:33:54 -0400

    oscpcommit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220608.2343';
+const cacheName = 'chirpy-20220711.2233';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
