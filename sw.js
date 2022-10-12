905f4fc54e9ef1733c32a36134d16baef95c1f8f
YuchaoZheng88 <yc236984611@gmail.com>
2022-10-12 04:12:19 -0400

    WinOffNote

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221009.1312';
+const cacheName = 'chirpy-20221012.1612';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
