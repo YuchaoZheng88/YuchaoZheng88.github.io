7fa883e4d1fc5d3cb5e005e76a6145791aaa236c
YuchaoZheng88 <yc236984611@gmail.com>
2022-10-16 01:43:47 -0400

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221012.1612';
+const cacheName = 'chirpy-20221016.1343';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
