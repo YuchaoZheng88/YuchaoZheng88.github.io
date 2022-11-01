6c1cbc225f47045a7a8c4cf92a2f7e415a64953c
YuchaoZheng88 <yc236984611@gmail.com>
2022-11-01 01:10:14 -0400

    networkplus

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221030.0048';
+const cacheName = 'chirpy-20221101.1310';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
