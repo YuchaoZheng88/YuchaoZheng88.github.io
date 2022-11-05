aeb5bbd506cb339c1f364893ee13b57d4114ad54
YuchaoZheng88 <yc236984611@gmail.com>
2022-11-05 11:39:17 -0400

    cehcert

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221101.1312';
+const cacheName = 'chirpy-20221105.2339';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
