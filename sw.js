4f27e261d668a9063a36a1e3147cbbddfbfc6e38
YuchaoZheng88 <yc236984611@gmail.com>
2022-11-07 13:58:57 -0500

    about pics arrange

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221108.0256';
+const cacheName = 'chirpy-20221108.0258';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
