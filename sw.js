87170cd5412d4f977da0ea4ce6aa53987a33d3bd
YuchaoZheng88 <yc236984611@gmail.com>
2022-11-26 11:15:05 -0500

    securityplus

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221108.0258';
+const cacheName = 'chirpy-20221127.0015';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
