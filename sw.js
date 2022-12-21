5101bd07abbd867b9e4b5b35a2be127adfb72203
YuchaoZheng88 <yc236984611@gmail.com>
2022-12-20 19:25:57 -0500

    chernocpp

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221219.0602';
+const cacheName = 'chirpy-20221221.0825';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
