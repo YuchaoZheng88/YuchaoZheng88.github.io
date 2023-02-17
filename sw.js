1c340af3a18e2b17fead30aae19909d0877f9de5
YuchaoZheng88 <yc236984611@gmail.com>
2023-02-17 02:37:18 -0500

    linux network

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230129.1339';
+const cacheName = 'chirpy-20230217.1537';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
