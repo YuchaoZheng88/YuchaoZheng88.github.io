079190e41f76b5ab2c1686f122a0b9afcffdf225
YuchaoZheng88 <yc236984611@gmail.com>
2023-03-09 12:13:17 -0500

    forensic and off

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230302.0513';
+const cacheName = 'chirpy-20230310.0113';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
