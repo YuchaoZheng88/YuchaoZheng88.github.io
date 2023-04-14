22eb22c10d7415d2e321956e0736b8af71415256
YuchaoZheng88 <yc236984611@gmail.com>
2023-04-14 16:43:22 -0400

    13cubed forensic

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230327.0226';
+const cacheName = 'chirpy-20230415.0443';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
