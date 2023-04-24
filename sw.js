7a386ea34431e48da13df897dc0ec137499706db
YuchaoZheng88 <yc236984611@gmail.com>
2023-04-24 00:53:33 -0400

    learnmalwareanalysis

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230415.0443';
+const cacheName = 'chirpy-20230424.1253';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
