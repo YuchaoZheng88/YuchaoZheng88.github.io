807035753ba75b7bca9872da8af789e63a0f55ba
YuchaoZheng88 <yc236984611@gmail.com>
2023-08-11 00:51:49 -0400

    case sichuan souce

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230430.0554';
+const cacheName = 'chirpy-20230811.1251';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
